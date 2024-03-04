4ac56aacc1ccee14d1820aebf7705e3d3f45200a
palabandlas <palabandla.srikanth@outlook.com>
2024-03-04 14:43:36 +0530

    deleted files

--- assets/scripts/app.js
+++
@@ -1,9 +0,0 @@
-const defaultResult = 0;
-const currentResult = defaultResult;
-function getUserNumberInput() {
-  return parseInt(userInput.value);
-}
-function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
-  calDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
-  outputResult(currentResult, calDescription);
-}
